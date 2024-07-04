736be0803acf7d3ebb0a54f48fb857a1708f0f91
Roman Aebi <unity2.sae@gmail.com>
2024-07-04 16:57:10 +0200

    EOC CCA scene setup plus UI setup

---
+++ Assets/Scripts/LevelManagement/UIs/CCAUI.cs
@@ -0,0 +1,43 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace LevelManagement.UIs {
+    public class CCAUI : MonoBehaviour
+    {
+        [SerializeField] private Button backToMenuButton;
+        [SerializeField] private EOCCCA eoc;
+        [SerializeField] private Toggle randomizingYesNo;
+        [SerializeField] private Slider stepsPerFrame;
+
+        
+
+        private void OnEnable() {
+            backToMenuButton.onClick.AddListener(OnClickBackToMenu);
+            randomizingYesNo.onValueChanged.AddListener(OnClickToggleRandomize);
+            stepsPerFrame.onValueChanged.AddListener(OnClickStepsPerFrameSlider);
+            
+        }
+
+        private void OnDisable() {
+            backToMenuButton.onClick.RemoveListener(OnClickBackToMenu);
+            randomizingYesNo.onValueChanged.RemoveListener(OnClickToggleRandomize);
+            stepsPerFrame.onValueChanged.RemoveListener(OnClickStepsPerFrameSlider);
+        }
+        
+        
+        private void OnClickBackToMenu() {
+            ScenesManager.Instance.LoadMainMenu();
+        }
+
+        private void OnClickToggleRandomize(bool yesNo) {
+                eoc.randomize = yesNo;
+            
+        }
+        
+        private void OnClickStepsPerFrameSlider(float value) {
+            eoc.stepsPerFrame = (int)value;
+        }
+        
+    }
+}
