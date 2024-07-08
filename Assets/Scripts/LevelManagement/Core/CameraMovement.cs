0a539b379658241f2b51a2007bfbb8f63688b7b0
Roman Aebi <unity2.sae@gmail.com>
2024-07-08 17:48:57 +0200

    Adjustments in the scene + cleaning

---
+++ Assets/Scripts/LevelManagement/Core/CameraMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace LevelManagement.Core {
+    public class CameraMovement : MonoBehaviour{
+        [SerializeField] private Transform anchor;
+        private float _multiplier;
+        private float _time;
+        private void Update() {
+            transform.RotateAround (anchor.transform.position, Vector3.up, 30 * Time.deltaTime * _multiplier);
+        }
+
+
+        public void SliderMultiplier(float adjustingSpeed) {
+            _multiplier = adjustingSpeed;
+        }
+    }
+}
