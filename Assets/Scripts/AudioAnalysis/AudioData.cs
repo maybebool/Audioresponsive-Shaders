5de164d9c47930f1c4a2d4ef67d549f918fd3b7d
Roman Aebi <unity2.sae@gmail.com>
2024-07-16 10:24:31 +0200

    refactoring AudioData.cs

--- Assets/Scripts/AudioAnalysis/AudioData.cs
+++ Assets/Scripts/AudioAnalysis/AudioData.cs
@@ -1,125 +1,175 @@
 using UnityEngine;
-using UnityEngine.Serialization;
 
 namespace AudioAnalysis {
-	
-	[RequireComponent (typeof (AudioSource))]
-	public class AudioData : MonoBehaviour {
-		[HideInInspector] public float[] audioBand;
-		[HideInInspector] public float[] audioBandBuffer;
-		[HideInInspector] public float amplitude;
-		[HideInInspector] public float amplitudeBuffer;
-		
-		[SerializeField] private AudioSource audioSource;
-		[SerializeField] private AudioClip audioClip;
-		
-		//FFT based values for Audio transformation
-		private float[] _samples = new float[512];
-		private float[] _frequencyBand = new float[8];
-		private float[] _bandBuffer = new float[8];
-		private float[] _bufferReduction = new float[8];
-		private float[] _frequencyBandHighest = new float[8];
+    public class SpectrumBandData {
+        public float[] FrequencyBand { get; } = new float[8];
+        public float[] BandBuffer { get; } = new float[8];
+        public float[] BufferReduction { get; } = new float[8];
+        public float[] FrequencyBandHighest { get; } = new float[8];
+    }
 
-		private float _AmplitudeHighest;
-		private float _audioProfile;
-
-
-		private void Start() {
-			_audioProfile = 0.5f;
-			audioBand = new float[8];
-			audioBandBuffer = new float[8];
-			AudioProfile(_audioProfile);
-			audioSource.clip = audioClip;
-			audioSource.Play();
-		}
+    [RequireComponent(typeof(AudioSource))]
+    public class AudioData : MonoBehaviour {
+        private SpectrumBandData _spectrumBandData = new();
+        [SerializeField] private int bandCount = 8;
+        [HideInInspector] public float[] audioBand;
+        [HideInInspector] public float[] audioBandBuffer;
+        [HideInInspector] public float amplitude;
+        [HideInInspector] public float amplitudeBuffer;
+        [SerializeField] private AudioSource audioSource;
+        [SerializeField] private AudioClip audioClip;
+        private float[] _samples = new float[512];
+        private float _amplitudeHighest;
+        private float _audioProfile;
 
 
-		private void Update() {
-			if (audioSource.clip == null) return;
-			GetAudioSpectrumData();
-			GenerateFrequencyFilters();
-			BandBuffer();
-			GenerateAudioBands();
-			GetAmplitude();
-		}
-
-		private void GetAudioSpectrumData() {
-			audioSource.GetSpectrumData(_samples, 0, FFTWindow.BlackmanHarris);
-		}
-
-		private void AudioProfile(float audioProfile) {
-			for (int i = 0; i < 8; i++) {
-				_frequencyBandHighest[i] = audioProfile;
-			}
-		}
-
-		private void GetAmplitude() {
-			float currentAmplitude = 0;
-			float currentAmplitudeBuffer = 0;
-			for (int i = 0; i < 8; i++) {
-				currentAmplitude += audioBand[i];
-				currentAmplitudeBuffer += audioBandBuffer[i];
-			}
-
-			if (currentAmplitude > _AmplitudeHighest) {
-				_AmplitudeHighest = currentAmplitude;
-			}
-
-			amplitude = currentAmplitude / _AmplitudeHighest;
-			amplitudeBuffer = currentAmplitudeBuffer / _AmplitudeHighest;
-		}
-
-		private void GenerateAudioBands() {
-			for (int i = 0; i < 8; i++) {
-				if (_frequencyBand[i] > _frequencyBandHighest[i]) {
-					_frequencyBandHighest[i] = _frequencyBand[i];
-				}
-
-				audioBand[i] = Mathf.Clamp((_frequencyBand[i] / _frequencyBandHighest[i]), 0, 1);
-				audioBandBuffer[i] = Mathf.Clamp((_bandBuffer[i] / _frequencyBandHighest[i]), 0, 1);
-			}
-		}
-		
-
-		private void BandBuffer() {
-			for (int i = 0; i < 8; ++i) {
-				if (_frequencyBand[i] > _bandBuffer[i]) {
-					_bandBuffer[i] = _frequencyBand[i];
-					//_bufferDecrease [g] = 0.005f;
-				}
-
-				if (_frequencyBand[i] < _bandBuffer[i] && _frequencyBand[i] > 0) {
-					_bufferReduction[i] = (_bandBuffer[i] - _frequencyBand[i]) / 8;
-					_bandBuffer[i] -= _bufferReduction[i];
-
-				}
-			}
-		}
+        private void Start() {
+            _audioProfile = 0.5f;
+            audioBand = new float[bandCount];
+            audioBandBuffer = new float[bandCount];
+            AudioProfile(_audioProfile);
+            audioSource.clip = audioClip;
+            audioSource.Play();
+        }
 
 
-		private void GenerateFrequencyFilters() {
-			int count = 0;
+        private void Update() {
+            if (audioSource.clip == null) return;
+            GetAudioSpectrumData();
+            GenerateFrequencyFilters();
+            CalculateBandBuffer();
+            GenerateAudioBands();
+            GetAmplitude();
+        }
 
-			for (int i = 0; i < 8; i++) {
+        private void GetAudioSpectrumData() {
+            audioSource.GetSpectrumData(_samples, 0, FFTWindow.BlackmanHarris);
+        }
+
+        private void AudioProfile(float audioProfile) {
+            for (int i = 0; i < bandCount; i++) {
+                _spectrumBandData.FrequencyBandHighest[i] = audioProfile;
+            }
+        }
+
+        /// <summary>
+        /// Calculates the amplitude of the audio, based on the audio bands.
+        /// </summary>
+        /// <remarks>
+        /// This method calculates the amplitude of the audio by summing up the values of the audio bands.
+        /// It also calculates the amplitude buffer, which is a smoothed version of the amplitude.
+        /// The resulting amplitudes are normalized between 0 and 1.
+        /// </remarks>
+        private void GetAmplitude() {
+            float currentAmplitude = 0;
+            float currentAmplitudeBuffer = 0;
+            for (int i = 0; i < bandCount; i++) {
+                currentAmplitude += audioBand[i];
+                currentAmplitudeBuffer += audioBandBuffer[i];
+            }
+
+            if (currentAmplitude > _amplitudeHighest) {
+                _amplitudeHighest = currentAmplitude;
+            }
+
+            amplitude = currentAmplitude / _amplitudeHighest;
+            amplitudeBuffer = currentAmplitudeBuffer / _amplitudeHighest;
+        }
+
+        /// <summary>
+        /// Generates audio bands based on the spectrum band data.
+        /// </summary>
+        /// <remarks>
+        /// This method iterates through each band and calculates the audio band
+        /// value by calling the CalculateBand method with the appropriate band index.
+        /// The calculated audio band values are stored in the audioBand array.
+        /// The audio band buffer values are stored in the audioBandBuffer array.
+        /// </remarks>
+        private void GenerateAudioBands() {
+            for (int i = 0; i < bandCount; i++) {
+                UpdateHighestFrequency(i);
+                audioBand[i] = CalculateBand(i, _spectrumBandData.FrequencyBand);
+                audioBandBuffer[i] = CalculateBand(i, _spectrumBandData.BandBuffer);
+            }
+        }
+
+        /// <summary>
+        /// Updates the highest frequency value for a given band index.
+        /// </summary>
+        /// <param name="i">The index of the band.</param>
+        private void UpdateHighestFrequency(int i) {
+            if (_spectrumBandData.FrequencyBand[i] > _spectrumBandData.FrequencyBandHighest[i]) {
+                _spectrumBandData.FrequencyBandHighest[i] = _spectrumBandData.FrequencyBand[i];
+            }
+        }
+
+        private float CalculateBand(int i, float[] band) {
+            return Mathf.Clamp((band[i] / _spectrumBandData.FrequencyBandHighest[i]), 0, 1);
+        }
 
 
-				float average = 0;
-				int sampleCount = (int)Mathf.Pow(2, i) * 2;
+        /// <summary>
+        /// Calculates the band buffer for each audio band based on the spectrum band data.
+        /// </summary>
+        /// <remarks>
+        /// This method iterates through each band and calculates the band buffer value
+        /// by comparing the current frequency band with the previous band buffer value.
+        /// If the current frequency band is greater than the previous band buffer value,
+        /// the band buffer is updated with the current frequency band.
+        /// If the current frequency band is less than the band buffer and greater than zero,
+        /// a buffer reduction is calculated and subtracted from the band buffer.
+        /// The band buffer values are stored in the SpectrumBandData object.
+        /// </remarks>
+        private void CalculateBandBuffer() {
+            for (int i = 0; i < bandCount; ++i) {
+                var frequencyBand = _spectrumBandData.FrequencyBand[i];
+                var bandBuffer = _spectrumBandData.BandBuffer[i];
 
-				if (i == 7) {
-					sampleCount += 2;
-				}
+                if (frequencyBand > bandBuffer) {
+                    _spectrumBandData.BandBuffer[i] = frequencyBand;
+                }
 
-				for (int j = 0; j < sampleCount; j++) {
-					average += (_samples [count] + _samples [count]) * (count + 1);
-					count++;
+                if (!(frequencyBand < bandBuffer) || !(frequencyBand > 0)) continue;
+                var bufferReduction = (bandBuffer - frequencyBand) / bandCount;
+                _spectrumBandData.BufferReduction[i] = bufferReduction;
+                _spectrumBandData.BandBuffer[i] -= bufferReduction;
+            }
+        }
 
-				}
 
-				average /= count;
-				_frequencyBand[i] = average * 10;
+        /// <summary>
+        /// Generates frequency filters based on the audio spectrum data.
+        /// </summary>
+        /// <remarks>
+        /// This method calculates the frequency filters that are used in audio analysis.
+        /// It iterates through each band and calculates the frequency band value by calling the CalculateSampleAverage method.
+        /// The calculated frequency band values are stored in the SpectrumBandData object.
+        /// </remarks>
+        private void GenerateFrequencyFilters() {
+            var count = 0;
+            for (int i = 0; i < bandCount; i++) {
+                var sampleCount = (int)Mathf.Pow(2, i) * 2;
+                if (i == 7) {
+                    sampleCount += 2;
+                }
+                _spectrumBandData.FrequencyBand[i] = CalculateSampleAverage(i, count, sampleCount) * 10;
+                count += sampleCount;
+            }
+        }
 
-			}
-		}
-	}
+        /// <summary>
+        /// Calculates the average value of the audio samples within a specified range.
+        /// </summary>
+        /// <param name="i">The index of the frequency band.</param>
+        /// <param name="start">The starting index of the audio samples.</param>
+        /// <param name="sampleCount">The number of audio samples to consider.</param>
+        /// <returns>The average value of the audio samples within the specified range.</returns>
+        private float CalculateSampleAverage(int i, int start, int sampleCount) {
+            float total = 0;
+            for (int j = start; j < start + sampleCount; j++) {
+                total += (_samples[j] + _samples[j]) * (j + 1);
+            }
+            return total / sampleCount;
+        }
+    }
 }
